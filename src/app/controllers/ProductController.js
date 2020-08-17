af81908ab83f5229fca65084e77eeec1bb21c245
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-08-17 14:59:29 -0300

    product registration ok

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,6 +1,46 @@
+const Category = require('../models/Category');
+const Product = require('../models/Product');
+
 module.exports = {
-  create(req, res) {
-    return res.render('products/create.njk');
+ async create(req, res) {
+    const results = await Category.all();
+    const categories = results.rows;
+
+      return res.render('products/create.njk', { categories });
+
+  },
+  async post(req, res) {
+    //Validação todos os campos obrigatórios
+    const keys = Object.keys(req.body)
+
+    for (const key of keys) {
+      if (req.body[key] == "")
+        return res.send("Por gentileza preencha todos os campos!")
+    }
+
+    let { category_id, user_id, name, description, old_price, price, quantity, status } = req.body;
+
+    price = price.replace(/\D/g, "");
+
+    const data = [
+      category_id,
+      user_id || 1,
+      name,
+      description,
+      old_price || price,
+      price,
+      quantity,
+      status || 1,
+    ];
+
+   let results = await Product.create(data);
+   const productId = results.rows[0].id;
+
+   results = await Category.all();
+   const categories = results.rows[0];
+   
+   return res.render('products/create.njk', { productId, categories });
+    
   }
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,12 +8,13 @@ routes.get('/', (req, res) => {
   return res.render('layout.njk');
 });
 
+//Alias
 routes.get('/ads/create', (req, res) => {
   return res.redirect('/products/create');
 });
 
 routes.get('/products/create', ProductController.create);
-
+routes.post('/products', ProductController.post);
 
 
 
