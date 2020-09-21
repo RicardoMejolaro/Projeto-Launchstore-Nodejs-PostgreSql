fbe3f86d9ad4a7130c0187cf334234588b68a645
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-09-21 20:03:30 -0300

    validação de email front-end

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -60,6 +60,45 @@ const Mask = {
   }
 }
 
+/*Validaçções*/
+const Validate = {
+  apply(input, func) {
+    Validate.clearErrors(input)
+
+    let results = Validate[func](input.value);
+    input.value = results.value;
+
+    if(results.error)
+      Validate.displayError(input, results.error);
+  },
+  displayError(input, error) {
+    const div = document.createElement('div');
+    div.classList.add('error');
+    div.innerHTML = error;
+    input.parentNode.appendChild(div);
+    input.focus();
+  },
+  clearErrors(input) {
+    const errorDiv = input.parentNode.querySelector('.error');
+
+    if(errorDiv)
+      errorDiv.remove();
+  },
+  isEmail(value) {
+    let error = null;
+    /*Expressão regular de validação de email */
+    const mailFormat = /^\w+([\.-_]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if(!value.match(mailFormat))
+      error = "Email inválido!"
+
+    return {
+      error,
+      value
+    }
+  }
+}
+
 const PhotosUpload = {
   input: "",
   preview: document.querySelector('#photos-preview'),
